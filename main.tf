27447b3e2ddeb07ec9e1f75f100f175764a9ea69
Walter J Montenegro <jesusmontenegro941@gmail.com>
2025-10-15 02:20:22 -0300

    Reorganizar la configuración de Terraform: mover recursos de clave y grupo de seguridad a la parte superior del archivo

--- main.tf
+++ main.tf
@@ -2,6 +2,39 @@ provider "aws" {
   region = "sa-east-1"
 }
 
+resource "aws_key_pair" "terraform-example-key" {
+  key_name   = "terraform-example-key"
+  public_key = file("./keys/terraform-test.key.pub")
+}
+
+resource "aws_security_group" "terraform-example-sg" {
+  name        = "terraform-example-sg"
+  description = "Security group for terraform example"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress = {
+    description = "Http"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "terraform-example" {
     ami = "ami-07c0cae188e21a093"
     instance_type = "t3.micro"
@@ -32,35 +65,3 @@ resource "aws_instance" "terraform-example" {
     key_name = aws_key_pair.terraform-example-key.key_name
 }
 
-resource "aws_key_pair" "terraform-example-key" {
-  key_name   = "terraform-example-key"
-  public_key = file("./keys/terraform-test.key.pub")
-}
-
-resource "aws_security_group" "terraform-example-sg" {
-  name        = "terraform-example-sg"
-  description = "Security group for terraform example"
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress = {
-    description = "Http"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
