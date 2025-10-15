6d7aa8278f3221674ad529d0ac43526cdaf3b4c4
Walter J Montenegro <jesusmontenegro941@gmail.com>
2025-10-15 02:18:48 -0300

    Agregar user_data para instalar Docker y Docker Compose en la instancia EC2

--- main.tf
+++ main.tf
@@ -6,6 +6,29 @@ resource "aws_instance" "terraform-example" {
     ami = "ami-07c0cae188e21a093"
     instance_type = "t3.micro"
 
+    user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              
+              # Instalar Docker
+              sudo amazon-linux-extras install docker -y
+              sudo service docker start
+              sudo usermod -a -G docker ec2-user
+              
+              # Instalar Docker Compose
+              sudo curl -L "https://github.com/docker/compose/releases/download/2.27.0/docker-compose-\$(uname -s)-\$(uname -m)" -o /usr/local/bin/docker-compose
+              sudo chmod +x /usr/local/bin/docker-compose
+              
+              # Clonar el repositorio con tu app Docker
+              cd /home/ec2-user
+              git clone https://github.com/MojeZcg/python-terraform-test.git app
+              cd app/app
+              
+              # Construir y ejecutar contenedor
+              sudo docker build -t python-flask-app .
+              sudo docker run -d -p 3000:3000 python-flask-app
+              EOF
+
     key_name = aws_key_pair.terraform-example-key.key_name
 }
 
