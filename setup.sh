2b034012a4ee597b2797da6cc63877e43a540f22
Walter J Montenegro <jesusmontenegro941@gmail.com>
2025-10-20 19:11:34 -0300

    Actualizar script de configuración para mejorar la instalación de Docker y Git en setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
-# Actualiza sistema e instala Docker
+set -e
+
+# Actualiza el sistema e instala Docker y Git
 sudo dnf update -y
+
+# Instala Docker
 sudo amazon-linux-extras enable docker
 sudo dnf install docker git -y
-sudo systemctl start docker
-sudo systemctl enable docker
+sudo systemctl enable --now docker
+
+# Agrega el usuario ec2-user al grupo docker para evitar usar sudo con Docker
 sudo usermod -aG docker ec2-user
 
-# Clonar tu repositorio de app Python (Flask/FastAPI)
+# Clona el repositorio de la aplicación
 cd /home/ec2-user
-git clone https://github.com/MojeZcg/python-terraform-test.git app
-cd app/app
+git clone https://github.com/MojeZcg/backend-terraform-test.git 
+
+# Configura Git para evitar problemas de permisos
+git config --global --add safe.directory /home/ec2-user/
 
 # Variables de conexión a la DB (inyectadas desde Terraform)
 echo "DATABASE_URL=${database_url}" > .env
 echo "BUCKET_NAME=${bucket_name}" >> .env
 
 # Construir y ejecutar Docker
+cd app/
 docker build -t python-flask-app .
 docker image prune -f
 docker run -d --restart always --env-file .env -p 3000:3000 --name py-app python-flask-app
